f895b2c4e08f584b2894853e29e331b0e7994f0c
gidwikintan <gidwikintan@gmail.com>
2021-10-02 22:51:28 +0700

    config eslint

---
+++ src/reducers/auth.js
@@ -0,0 +1,18 @@
+const type = {
+
+};
+
+const initialState = {
+
+};
+
+export default (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    default:
+      return newState;
+  }
+};
+
+export const LOGIN = (authData) => ({ type: type.LOGIN, payload: authData });
+export const LOGOUT = () => ({ type: type.LOGOUT, payload: '' });
